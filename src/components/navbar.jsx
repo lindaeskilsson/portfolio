7c2d96ff1c3297c04c6b247e36055a2f2867b158
Linda Eskilsson <eskilsson.linda@hotmail.se>
2026-04-27 13:27:09 +0200

    chanve PDF

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import cvPdf from "../assets/Linda-Eskilsson-cv.docx.pdf";
+import cvPdf from "../assets/Linda-Eskilsson-LIA-CV.pdf";
 
 export default function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
